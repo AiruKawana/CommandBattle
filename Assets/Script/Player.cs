a0a2e2f474024338b03a7563b245aa4cb778d5e7
kawanaairu <airu92256@gmail.com>
2024-07-10 09:34:42 +0900

    スクリプトの追加と修正

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,11 +4,44 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] StatusData statusdata;
+    [SerializeField] private StatusData statusData;
+
+    [SerializeField] private int playerHp;
+    [SerializeField] private int playerSp;
+    private int skillCount = 0;
+    private int itemCount = 5;
     // Start is called before the first frame update
     void Start()
     {
+        playerHp = statusData.Hp;
+        playerSp = statusData.Sp;
+        MyItem();
+    }
+
+    public void AttackEnemy()
+    {
         
+    }
+
+    public void Skill()
+    {
+        if(playerSp > skillCount)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                
+            }
+        }
+    }
+
+    public void MyItem()
+    {
+        if (itemCount > 0)
+        {
+            --itemCount;
+        }
+        
+
     }
 
     // Update is called once per frame
